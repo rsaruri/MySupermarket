762c1ad8d4c4d14d665747d93e97d4a04f9fdf35
rsaruri <67058779+rsaruri@users.noreply.github.com>
2021-11-16 23:28:10 +0200

    Add files via upload

--- WebApplication2/ScriptFiles/JavaScript.js
+++ WebApplication2/ScriptFiles/JavaScript.js
@@ -1,8 +1,11 @@
 ﻿
-
+//open new html page for login 
 document.getElementById("btnLogin").onclick = window.open("file:///C:/Users/Rina%20Saruri/source/repos/WebApplication2/WebApplication2/HtmlPages/LoginPage.html");
 
-function createProduct(id, name, imageSource) {
+//-----------------
+
+
+/*function createProduct(id, name, imageSource) {
     let myProduct = {
         id: id,
         name: name,
@@ -11,12 +14,30 @@ function createProduct(id, name, imageSource) {
     return myProduct;
 }
 
-myImage = new Image();
-Image.src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bamba.JPG';
+
+var imgArray = new Array();
+imgArray[0] = new Image();
+imgArray[0].src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bamba.JPG';
+imgArray[1] = new Image();
+imgArray[1].src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\chips.JPG';
+imgArray[2] = new Image();
+imgArray[2].src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bisli.JPG';
+
+let result1 = createProduct(1, 'bamba', imgArray[0].src)
+console.log(result1);
+let result2 = createProduct(2, 'chips', imgArray[1].src)
+console.log(result2);
+let result3 = createProduct(3, 'bisli', imgArray[2].src)
+console.log(result3);*/
 
 
-let result = createProduct(1, 'aaa', myProductImage.src);
-console.log(result);
+let img = document.createElement("img");
+img.src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bamba.JPG';
+
+img.style.border = "0.5px solid orange";
+img.style.borderRadius = "100px";
+
+document.body.appendChild(img);
 
 
 
