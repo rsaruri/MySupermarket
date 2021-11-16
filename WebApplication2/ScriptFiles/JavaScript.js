688e4be7dde162abc447831cbdafb8aa19b81a81
rsaruri <67058779+rsaruri@users.noreply.github.com>
2021-11-16 17:02:24 +0200

    Add files via upload

---
+++ WebApplication2/ScriptFiles/JavaScript.js
@@ -0,0 +1,23 @@
+﻿
+
+document.getElementById("btnLogin").onclick = window.open("file:///C:/Users/Rina%20Saruri/source/repos/WebApplication2/WebApplication2/HtmlPages/LoginPage.html");
+
+function createProduct(id, name, imageSource) {
+    let myProduct = {
+        id: id,
+        name: name,
+        imageSource: imageSource,
+    };
+    return myProduct;
+}
+
+myImage = new Image();
+Image.src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bamba.JPG';
+
+
+let result = createProduct(1, 'aaa', myProductImage.src);
+console.log(result);
+
+
+
+
