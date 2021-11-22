e0b66dc3a1762eafd69658ac86584beb00b4ff71
rsaruri <67058779+rsaruri@users.noreply.github.com>
2021-11-22 10:27:15 +0200

    Add files via upload

--- WebApplication2/ScriptFiles/JavaScript.js
+++ WebApplication2/ScriptFiles/JavaScript.js
@@ -3,8 +3,25 @@
 document.getElementById("btnLogin").onclick = window.open("file:///C:/Users/Rina%20Saruri/source/repos/WebApplication2/WebApplication2/HtmlPages/LoginPage.html");
 
 
+//option 1: Create array object
 
-//option 1: Show 3 imagas from array
+let productsArray = [
+    { id: 1, name: 'Bamba', price: 5, source: 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bamba.JPG' },
+    { id: 2, name: 'chips', price: 4, source: 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\chips.JPG' },
+    { id: 3, name: 'Bisli', price: 3, source: 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bisli.JPG' },
+];
+
+//sum of price products
+const productSumPrice = productsArray.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
+console.log(productSumPrice);
+document.createElement 
+
+//show the source of image
+let productSource = productsArray.map(item => item.source);
+console.log(productSource);
+
+
+//option 2: Show 3 imagas from array
 /*function createProduct(id, name, imageSource) {
     let myProduct = {
         id: id,
@@ -31,13 +48,14 @@ console.log(result3);*/
 
 
 
-//option 2: Show 1 image 
-let img = document.createElement("img");
+//option 3: Show 1 image 
+
+/*let img = document.createElement("img");
 img.src = 'C:\Users\Rina Saruri\source\repos\WebApplication2\picture\bamba.JPG';
 img.style.border = "0.5px solid orange";
 img.style.borderRadius = "100px";
+document.body.appendChild(img);*/
 
-document.body.appendChild(img);
 
 
 
